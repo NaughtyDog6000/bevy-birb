4afa8c7d4a7e52d6c4198619c7e4e8cf37b4c0e7
NaughtyDog6000 <108585554+NaughtyDog6000@users.noreply.github.com>
2024-02-20 16:15:40 +0000

    wasm build

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BevyBirb
 
 Creating Flappy Bird Clone in bevy for both wasm and native platforms
+
+wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "bevy_birb" target/wasm32-unknown-unknown/release/bevy_birb.wasm
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world?!");
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -10,7 +10,7 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, spawn_player))
         .run();
 }
 
@@ -18,4 +18,29 @@ fn setup(mut commands: Commands) {
     let mut camera_bundle = Camera2dBundle::default();
     camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.0);
     commands.spawn(camera_bundle);
+}
+
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        Player,
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.0, 1.0, 0.0),
+                custom_size: Some(Vec2::new(1.0, 2.0)),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+    ));
+}
+
+fn move_player(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+    
 }
