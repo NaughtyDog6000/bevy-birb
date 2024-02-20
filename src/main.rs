13441c8966efe08fae7e2ed87db1fa9cd991cb71
NaughtyDog6000 <108585554+NaughtyDog6000@users.noreply.github.com>
2024-02-20 13:24:32 +0000

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 fn main() {
     println!("Hello, world!");
-    App::new().run();
-    
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.0);
+    commands.spawn(camera_bundle);
 }
